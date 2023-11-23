a30ec54c7f3705beb157e33d5d224f88272159e0
Harminder Virk <virk.officials@gmail.com>
2023-11-23 13:08:59 +0530

    style: format source code

--- config/app.ts
+++ config/app.ts
@@ -1,5 +1,5 @@
 import env from '#start/env'
-import app from '@adonisjs/core/services/app';
+import app from '@adonisjs/core/services/app'
 import { defineConfig } from '@adonisjs/core/http'
 
 /**
